fcc92eba05f4eedb792a6b22a02a5e8093aa2181
Luan Fernandes <luan.fj01@gmail.com>
2020-03-12 20:01:16 -0300

    Add Order Items migration

---
+++ backend/database/migrations/1584053883595_create_order_item_schema.js
@@ -0,0 +1,35 @@
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema');
+
+class CreateOrderItemSchema extends Schema {
+  up() {
+    this.create('order_items', table => {
+      table.increments();
+      table
+        .integer('product_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('products')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table.integer('quantity').unsigned();
+      table.decimal('subtotal');
+      table
+        .integer('order_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('orders')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+      table.timestamps();
+    });
+  }
+
+  down() {
+    this.drop('order_items');
+  }
+}
+
+module.exports = CreateOrderItemSchema;
