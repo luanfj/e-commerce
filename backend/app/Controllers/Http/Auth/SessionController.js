09a5de5ca33343db73bd02348d546f6ece6ea122
Luan Fernandes <luan.fj01@gmail.com>
2020-03-13 19:45:01 -0300

    Add Sign in & Sign up in controllers

--- backend/app/Controllers/Http/Auth/SessionController.js
+++ backend/app/Controllers/Http/Auth/SessionController.js
@@ -1,5 +1,11 @@
 class SessionController {
-  async store({ request, response }) {}
+  async store({ request, response, auth }) {
+    const { email, password } = request.all();
+
+    const data = await auth.withRefreshToken().attempt(email, password);
+
+    return response.send({ data });
+  }
 }
 
 module.exports = SessionController;
--- backend/app/Controllers/Http/Auth/UserController.js
+++ backend/app/Controllers/Http/Auth/UserController.js
@@ -1,5 +1,39 @@
+const Database = use('Database');
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User');
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Role = use('Role');
+
 class UserController {
-  async store({ request, response }) {}
+  async store({ request, response }) {
+    const trx = await Database.beginTransaction();
+
+    try {
+      const { name, surname, email, password } = request.all();
+
+      const user = await User.create(
+        {
+          name,
+          surname,
+          email,
+          password,
+        },
+        trx
+      );
+
+      const clientRole = Role.findBy('slug', 'client');
+
+      await user.roles().attach([clientRole.id], null, trx);
+
+      await trx.commit();
+
+      return response.status(201).send({ data: user });
+    } catch (error) {
+      await trx.rollback();
+
+      return response.status(400).send({ message: 'An error has occurred.' });
+    }
+  }
 }
 
 module.exports = UserController;
