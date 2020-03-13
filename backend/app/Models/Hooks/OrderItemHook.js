4c037d5b1f75de755197e9b48068a734b16c45fc
Luan Fernandes <luan.fj01@gmail.com>
2020-03-13 18:20:50 -0300

    Create OrderItemHook

---
+++ backend/app/Models/Hooks/OrderItemHook.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-multi-assign
+const OrderItemHook = (exports = module.exports = {});
+
+const Product = use('App/Models/Product');
+
+OrderItemHook.updateSubtotal = async model => {
+  const product = await Product.find(model.product_id);
+
+  model.subtotal = model.quantity * product.price;
+};
