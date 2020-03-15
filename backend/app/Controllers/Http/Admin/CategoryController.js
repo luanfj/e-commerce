f99295f42d985ab7dd35a35407ad98d2d6274add
Luan Fernandes <luan.fj01@gmail.com>
2020-03-15 13:19:41 -0300

    Add store in categories for admin

--- backend/app/Controllers/Http/Admin/CategoryController.js
+++ backend/app/Controllers/Http/Admin/CategoryController.js
@@ -1,3 +1,4 @@
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Category = use('App/Models/Category');
 
 class CategoryController {
@@ -13,6 +14,20 @@ class CategoryController {
     const categories = await query.paginate(pagination.page, pagination.limit);
 
     return response.send(categories);
+  }
+
+  async store({ request, response }) {
+    try {
+      const data = request.only(['title', 'description', 'image_id']);
+
+      const category = await Category.create(...data);
+
+      return response.status(201).send(category);
+    } catch (error) {
+      return response
+        .status(400)
+        .send({ message: 'Error processing your request' });
+    }
   }
 }
 
