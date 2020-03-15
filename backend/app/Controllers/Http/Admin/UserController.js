8a5d3e4aa0ba9a98a1e13ff274d0cfa05fc312c2
Luan Fernandes <luan.fj01@gmail.com>
2020-03-15 14:01:09 -0300

    Add CRUD Users for admin

--- backend/app/Controllers/Http/Admin/UserController.js
+++ backend/app/Controllers/Http/Admin/UserController.js
@@ -1,62 +1,86 @@
-/** @typedef {import('@adonisjs/framework/src/Request')} Request */
-/** @typedef {import('@adonisjs/framework/src/Response')} Response */
-/** @typedef {import('@adonisjs/framework/src/View')} View */
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const User = use('App/Models/User');
 
-/**
- * Resourceful controller for interacting with users
- */
 class UserController {
-  /**
-   * Show a list of all users.
-   * GET users
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index({ request, response, view }) {}
+  async index({ request, response, pagination }) {
+    const name = request.input('name');
 
-  /**
-   * Create/save a new user.
-   * POST users
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store({ request, response }) {}
+    const query = User.query();
 
-  /**
-   * Display a single user.
-   * GET users/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show({ params, request, response, view }) {}
+    if (name) {
+      query.where('name', 'ILIKE', `%${name}%`);
+      query.orWhere('surname', 'ILIKE', `%${name}%`);
+      query.orWhere('email', 'ILIKE', `%${name}%`);
+    }
 
-  /**
-   * Update user details.
-   * PUT or PATCH users/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update({ params, request, response }) {}
+    const users = await query.paginate(pagination.page, pagination.limit);
 
-  /**
-   * Delete a user with id.
-   * DELETE users/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy({ params, request, response }) {}
+    return response.send(users);
+  }
+
+  async store({ request, response }) {
+    try {
+      const data = request.only([
+        'name',
+        'surname',
+        'email',
+        'password',
+        'image_id',
+      ]);
+
+      const user = await User.create({ ...data });
+
+      return response.status(201).send(user);
+    } catch (error) {
+      return response
+        .status(400)
+        .send({ message: 'Error processing your request.' });
+    }
+  }
+
+  async show({ params: { id }, response }) {
+    const user = await User.findOrFail(id);
+
+    return response.send(user);
+  }
+
+  async update({ params: { id }, request, response }) {
+    const user = await User.findOrFail(id);
+
+    try {
+      const data = request.only([
+        'name',
+        'surname',
+        'email',
+        'password',
+        'image_id',
+      ]);
+
+      user.merge({ ...data });
+
+      await user.save();
+
+      return response.send(user);
+    } catch (error) {
+      return response
+        .status(400)
+        .send({ message: 'Error processing your request.' });
+    }
+  }
+
+  async destroy({ params: { id }, response }) {
+    const user = await User.findOrFail(id);
+
+    try {
+      await user.delete();
+
+      return response.status(204).send();
+    } catch (error) {
+      return response
+        .status(500)
+        .send({ message: 'Error processing your request.' });
+    }
+  }
 }
 
 module.exports = UserController;
