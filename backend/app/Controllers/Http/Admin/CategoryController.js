0007990a5d01442f8071101c1e4bc89688187c0e
Luan Fernandes <luan.fj01@gmail.com>
2020-03-15 13:14:34 -0300

    Add search in Categories for admin

--- backend/app/Controllers/Http/Admin/CategoryController.js
+++ backend/app/Controllers/Http/Admin/CategoryController.js
@@ -2,57 +2,18 @@ const Category = use('App/Models/Category');
 
 class CategoryController {
   async index({ request, response, pagination }) {
-    const page = request.input('page');
-    const limit = request.input('limit');
+    const title = request.input('title');
 
-    const categories = await Category.query().paginate(
-      pagination.page,
-      pagination.limit
-    );
+    const query = Category.query();
+
+    if (title) {
+      query.where('title', 'ILIKE', `%${title}%`);
+    }
+
+    const categories = await query.paginate(pagination.page, pagination.limit);
 
     return response.send(categories);
   }
-
-  /**
-   * Create/save a new category.
-   * POST categories
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async store({ request, response }) {}
-
-  /**
-   * Display a single category.
-   * GET categories/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show({ params, request, response, view }) {}
-
-  /**
-   * Update category details.
-   * PUT or PATCH categories/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update({ params, request, response }) {}
-
-  /**
-   * Delete a category with id.
-   * DELETE categories/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy({ params, request, response }) {}
 }
 
 module.exports = CategoryController;
